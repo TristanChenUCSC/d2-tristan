9ada9ada770b66d5e6124c45aea9b85569e0151d
TristanChenUCSC <trzchen@ucsc.edu>
2025-10-17 00:38:55 +0000

    Added title and canvas (step 1 complete)

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,15 @@
-import exampleIconUrl from "./noun-paperclip-7598668-00449F.png";
 import "./style.css";
 
 document.body.innerHTML = `
-  <p>Example image asset: <img src="${exampleIconUrl}" class="icon" /></p>
 `;
+
+// Title
+const title = document.createElement("h1");
+title.textContent = "Sticker Sketchpad";
+document.body.append(title);
+
+// Canvas
+const canvas = document.createElement("canvas");
+canvas.width = 256;
+canvas.height = 256;
+document.body.append(canvas);
